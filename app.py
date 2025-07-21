76367f8bd1bf49ad23896b6155968f4da7ba0f3d
rdl-ccd <rdl-ccd@bni.co.id>
2025-07-21 16:00:36 +0700

    Add App UI

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Document Generator Hub",
+    layout="centered"
+)
+
+st.title("📄 Document Generator Hub")
+
+st.write("""
+Welcome to the Document Generator Hub. 
+
+Please select a generator from the sidebar on the left to get started.
+""")
+
+st.info("👈 **Choose either the FSD Generator or the Unit Test Generator from the sidebar.**")
